25f10340ff99c6d03f80b58d086d515127959134
project52 <joshua.p.cai.python@gmail.com>
2015-04-13 18:43:06 +0000

    flask-hello

---
+++ app.py
@@ -0,0 +1,19 @@
+#--------------Flask Hello World-----------#
+import os
+#import the Flask class from flask module
+from flask import Flask
+
+#create the application object
+app = Flask(__name__)
+
+#use decorators to link the function to an url
+@app.route("/")
+@app.route("/hello")
+
+# define the view using a function, which returns a string
+def hello_world():
+    return "Hello World!"
+
+#start the develepment server using the run() method
+if __name__ == "__main__":
+    app.run(host=os.environ['IP'],port=int(os.environ['PORT']))
